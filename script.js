a80a3bbe07e068702ff803e2514699dc8cb190a2
rajeshmondal54 <rm4223536@gmail.com>
2024-06-17 23:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const links = {
+    bruteForce: {
+        href: "https://acciojob.com/blog/brute-force-vs-optimization",
+        article_name: "Brute Force vs. Optimization"
+    },
+    dsa: {
+        href: "https://acciojob.com/blog/dsa-vs-competitive-programming",
+        article_name: "DSA vs. Competitive Programming"
+    },
+    flexbox: {
+        href: "https://acciojob.com/blog/complete-guide-on-flexbox",
+        article_name: "Complete Guide on FlexBox"
+    }
+};
+
+Object.values(links).forEach(({ href, article_name }) => {
+    cy.contains(article_name)
+        .parent() 
+        .find('a')
+        .should('have.attr', 'href', href);
+});
